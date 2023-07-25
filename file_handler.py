81c081b62a4e3477ccb7c4d587c2db642827d90e
Ebby Samson <ebbysamson@gmail.com>
2023-07-25 07:46:37 +0100

    Added inst. file generation.

--- file_handler.py
+++ file_handler.py
@@ -20,10 +20,10 @@ class FileHandler:
 
     def addDeclr(self, pos_string):
         self.internal.append('p0_' + pos_string)
-        self.defs['p0_' + pos_string] = []
+        self.defs['p0_' + pos_string] = set()
 
     def addDef(self, pos_string, line):
-        self.defs['p0_' + pos_string].append(line)
+        self.defs['p0_' + pos_string].add(line)
 
     def writeFile(self):
 
@@ -37,13 +37,14 @@ class FileHandler:
         handle.write('\t// Declare input signals.\n')
 
         for line in self.inputs:
-            handle.write('\tinput  logic ' + line + ';\n')
+            handle.write('\tinput  logic ' + line + ',\n')
 
         # Declare outputs
         handle.write('\t// Declare output signals.\n')
 
-        for line in self.outputs:
-            handle.write('\toutput logic ' + line + ';\n')
+        for line in self.outputs[:-1]:
+            handle.write('\toutput logic ' + line + ',\n')
+        handle.write('\toutput logic ' + self.outputs[-1] + '\n')
 
         # Close header
         handle.write(');\n\n')
@@ -51,7 +52,10 @@ class FileHandler:
         # Declare internal signals
         handle.write('\t// Declare internal signals.\n')
         for line in self.internal:
-            handle.write('\tlogic [2:0] ' + line + ';\n')
+            if line[-1] == 'w':
+                handle.write('\tlogic       ' + line + ';\n')
+            else:
+                handle.write('\tlogic [2:0] ' + line + ';\n')
 
         handle.write('\n')
 
@@ -78,4 +82,44 @@ class FileHandler:
         handle.write('\nendmodule\n')  # End module definition
 
         handle.close()  # Write to output file and close
+
+        self.writeInstFile()
+
+
+    def writeInstFile(self):
+        
+        inputs = []
+        outputs = []
+
+        namefile = open(self.name.split('.')[0]+'_names.rs')
+
+        for line in [x.replace('\n','').split(' ') for x in namefile.readlines()]:
+            if line[0] == "input":
+                inputs.append(line[1])
+            elif line[0] == "output":
+                outputs.append(line[1])
+
+        # Create the file.
+        handle = open(self.name.split('.')[0]+'_names.sv','w')
+
+        # Write header
+        handle.write(self.name.split('.')[0]+' inst_gen (\n')
+
+        # Declare inputs
+        handle.write('\t// Declare input signals.\n')
+
+        for i,line in enumerate(self.inputs):
+            if(len(inputs) > i):
+                handle.write('\t.' + line + '(' + inputs[i] + '),\n')
+
+        # Declare outputs
+        handle.write('\t// Declare output signals.\n')
+
+        for i,line in enumerate(self.outputs[:-1]):
+            if(len(outputs) > i):
+                handle.write('\t.' + line + '(' + outputs[i] + '),\n')
+        handle.write('\t.' + self.outputs[-1] + '(' + outputs[len(self.outputs)-1] + ')\n')
+
+        # Close header
+        handle.write(');\n\n')
 
